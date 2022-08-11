79683a513a39948f157ee81e8904a154989dacc2
hibecung123 <son.nguyen1612@hcmut.edu.vn>
2022-08-11 15:10:46 +0700

    Features

--- script.js
+++ script.js
@@ -2,9 +2,44 @@ const squares=document.querySelectorAll('.square')
 const mole=document.querySelector('.mole')
 const timeleft=document.querySelector('#Time')
 const score=document.querySelector('#Score')
-
+const choose=document.querySelectorAll(".Difficulty")
+const dif=document.getElementById('Diff')
 let res=0
 let hitposition
+let mytime
+let choosing=null
+let timeup
+let starttime
+let timing
+document.getElementById('Start').onclick=function()
+{
+    timeup=window.prompt("How long you want to play?")
+    if(choosing===null)
+    {
+        alert('You are not choosing the Difficulty')
+    }
+    else if(choosing===Easy)
+    {
+        timing=900
+        start()
+    }
+    else if(choosing===Medium)
+    {
+        timing=700
+        start()
+    }
+    else
+    {
+        timing=500
+        start()
+    }
+    
+}
+function start()
+{
+    mytime=setInterval(random,timing)
+    starttime=setInterval(count,1000)
+}
 function random()
 {
     squares.forEach(squares=>
@@ -28,23 +63,19 @@ squares.forEach(square=>
         }
     })
 })
-let mytime
-let timeup=window.prompt("Nhap so giay ban muon choi")
-let starttime
-function start()
-{
-    mytime=setInterval(random,600)
-    starttime=setInterval(count,1000)
-}
-start()
 function count()
 {
-    
     timeleft.textContent=timeup
     timeup--;
     if(timeup==-1)
     {
         clearInterval(starttime)
-        alert('Ban duoc so diem la: '+res)
+        alert('Your score is: '+res)
+        setTimeout("location.reload(true);",1000)
     }
 }
+choose.forEach(choose=> choose.addEventListener('click',(e)=>
+{
+    choosing=e.target.id
+    dif.innerHTML=choosing
+}))
